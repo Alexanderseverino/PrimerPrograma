a22c215f01bc15b9774b07c06179607b2e46b12e
alexander de jesus severino peña <as23-0329@unphu.edu.do>
2024-01-19 10:46:26 -0400

    primer commit

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        String nombre = "Alexander Severino";
+
+        double monto = 475.50;
+
+        double procentajeadicional = 0.15;
+
+        double resultado = monto * (1 + procentajeadicional);
+
+        System.out.println("Bienvenido" + nombre + "El total fue de" + resultado);
+    }
+
+}
