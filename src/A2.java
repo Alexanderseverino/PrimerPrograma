49093d6c2eb128c23a3c500bdaeebfa55607b3a9
alexander de jesus severino peña <as23-0329@unphu.edu.do>
2024-01-20 13:30:10 -0400

    primer commit

---
+++ src/A2.java
@@ -0,0 +1,22 @@
+public class A2 {
+    public static void main(String[] args) {
+
+        String tipo = "Computadora";
+
+        double TASA_DOLAR = 59.50;
+
+        boolean disponible = false;
+
+        String MENSAJE_BIENVENIDA = "Bienvenido a este módulo";
+
+        int resultado = 4 * 9;
+
+        double monto = 775.49;
+
+        double impuesto = 0.18;
+
+        double montoConITBIS = monto * (1 + impuesto);
+
+        System.out.println(" El monto con ITBIS es: " + montoConITBIS);
+    }
+}
